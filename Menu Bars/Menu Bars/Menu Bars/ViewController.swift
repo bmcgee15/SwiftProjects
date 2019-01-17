240d0148d19ea266463241e7a9ff4eced625c4b5
Bret McGee <bret.mcgee15@gmail.com>
2019-01-17 13:18:43 -0500

    Add files via upload
    initial commit did not bring everything

---
+++ Menu Bars/Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Bret McGee on 2018-06-06.
+//  Copyright © 2018 Bret McGee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func camera(_ sender: Any) {
+        
+        print("Camera Button is Pressed")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
