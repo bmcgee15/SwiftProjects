9e12cbcde5cafaf62c9c9dbb00fbbd2e3102f30c
Bret McGee <bret.mcgee15@gmail.com>
2019-01-17 13:20:33 -0500

    Add files via upload
    initial commit did not bring everything

---
+++ multi-view/multi-view-controller/multi-view-controller/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  multi-view-controller
+//
+//  Created by Bret McGee on 2018-06-25.
+//  Copyright © 2018 Bret McGee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Second View Controller Loaded")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ multi-view/multi-view-controller/multi-view-controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multi-view-controller
+//
+//  Created by Bret McGee on 2018-06-25.
+//  Copyright © 2018 Bret McGee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
